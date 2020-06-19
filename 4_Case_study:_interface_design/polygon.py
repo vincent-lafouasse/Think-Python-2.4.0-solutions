18c4e2000f0b45c2e6791396f7ccbbad71d35fdf
vincent-lafouasse <vincentlafouasse@live.fr>
2020-06-19 02:27:00 +0200

    linting ch4

--- 4_Case_study:_interface_design/polygon.py
+++ 4_Case_study:_interface_design/polygon.py
@@ -4,35 +4,40 @@ import math
 bob = turtle.Turtle()
 tau = 2 * math.pi
 
-def polygon(t, n = 4, length = 100):
+
+def polygon(t, n=4, length=100):
     for _ in range(n):
         t.fd(length)
         t.lt(360/n)
 
-def circle(t, r, N = 100):
+
+def circle(t, r, N=100):
     polygon(t, tau * r / N, N)
 
-def arc(t, r, angle, N = 100):
+
+def arc(t, r, angle, N=100):
     rad = tau * angle / 360
     n = math.floor(N * rad / tau)
-    l = tau * r / N
+    length = tau * r / N
 
     for _ in range(n):
-        t.fd(l)
+        t.fd(length)
         t.lt(360/N)
 
-def isoceles(t, angle1, length = 100):
+
+def isoceles(t, angle1, length=100):
     angle1_rad = angle1 * tau / 360
     angle2 = (180. - angle1) / 2.
     L = 2 * length * math.sin(angle1_rad / 2)
-    
+
     t.fd(length)
     t.lt(180 - angle2)
     t.fd(L)
     t.lt(180 - angle2)
     t.fd(length)
 
-def pie(t, N, length = 100):
+
+def pie(t, N, length=100):
     for _ in range(N):
         isoceles(t, 360/N, length)
         t.lt(180)
@@ -42,4 +47,3 @@ number = int(input())
 pie(bob, number)
 
 turtle.mainloop()
-
